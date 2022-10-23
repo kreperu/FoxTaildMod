1526788a771d4fda8281edc1bfb04abe2b552154
NayFeex <86920840+kreperu@users.noreply.github.com>
2022-10-23 15:00:35 +0200

    Delete src/main directory

--- src/main/java/me/nayfeex/playerrendererfunidk/client/util/Scale.java
+++
@@ -1,7 +0,0 @@
-package me.nayfeex.playerrendererfunidk.client.util;
-
-public class Scale {
-    public static float map(float val, float src1, float src2, float dst1, float dst2) {
-        return ((val - src1) / (src2 - src1)) * (dst2 - dst1) + dst1;
-    }
-}
